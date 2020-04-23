ea6ec5786f8c0d13aadfb379a756a1d1504f6d58
CavLuni <denis.luniere@gmail.com>
2020-04-23 17:03:15 -0300

    Project Create

---
+++ src/test/java/com/cavluni/wsmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavluni.wsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
